95372d70829059d0fee2cc7cab2d89498e617146
justinbhalla <justinbhalla28@gmail.com>
2020-11-30 22:17:56 -0500

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -12,8 +12,8 @@ module.exports = {
 
     // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
     // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
-    mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@realjustinlev",
-   // mediumRssFeed: "https://medium.com/feed/@justinbhalla28",
+    //mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@realjustinlev",
+    mediumRssFeed: "https://medium.com/feed/@realjustinlev",
     shownArticles: 3,
     
     // There are icons available for the following platforms: 
